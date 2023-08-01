5a839aa0aac3fba06c72f354d2754227df0dc2f6
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-01 11:31:57 -0700

    advanced tasks

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+#sender = ARGV[0].match(/\[from:(.*?)\]/)&.captures&.first
+#receiver = ARGV[0].match(/\[to:(.*?)\]/)&.captures&.first
+#flags = ARGV[0].match(/\[flags:(.*?)\]/)&.captures&.first
+#puts "#{sender},#{receiver},#{flags}"
