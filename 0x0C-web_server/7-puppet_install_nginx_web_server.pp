2b07bb1dfa45976236b6522ebfb078599cb6e44a
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-26 18:16:15 -0700

    mandatory tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,5 +4,6 @@ exec {'install Nginx web server':
   provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
   echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ;
-  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/OluwaninsolaAO permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/OluwaninsolaAO permanent;/" /etc/nginx/sites-available/default ;
+  sudo service nginx start',
 }
