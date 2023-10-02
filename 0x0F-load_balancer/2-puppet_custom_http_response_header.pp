1633d71f0bbd9c05a886675a16eea9b629fd9c26
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-02 16:02:21 -0700

    advanced tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #puppet custom http response header
 
-exec { 'HTTP header':
+exec {'custom HTTP':
     command  => 'apt-get -y update;
-    apt-get install -y nginx;
     sudo sed -i "50i\\t\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default;
+	apt-get install -y nginx;
     service nginx restart',
     provider => shell
 }
