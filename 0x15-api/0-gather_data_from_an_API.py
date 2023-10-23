eeb98987dcd540ae1173983a8dcbd9eeb2023292
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-23 15:34:44 -0700

    mandatory

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" Python script that using this REST API"""
+"""Python script that using this REST API"""
 import json
 from sys import argv
 from urllib import request
 
 
-def usr(argv):
+def get_user(argv):
     """this function do most of the job"""
-    tasks_id = request.urlopen(
+    tsks_id = request.urlopen(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
     user_name = request.urlopen(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
-    res_body = tasks_id.read()
+    res_body = tsks_id.read()
     user_body = user_name.read()
     json_dict = json.loads(res_body.decode("utf-8"))
     json_user = json.loads(user_body.decode("utf-8"))
@@ -19,13 +19,13 @@ def usr(argv):
     for i in range(0, len(json_dict)):
         if not json_dict[i]['completed']:
             count += 1
-           print('Employee {} is done with tasks({}/{}):'
-                   .format(json_user['name'], len(json_dict) - count, len(json_dict)))
-    for ix in range(0, len(json_dict)):
-        if json_dict[ix]['completed']:
+    print('Employee {} is done with tasks({}/{}):'
+          .format(json_user['name'], len(json_dict) - count, len(json_dict)))
+    for idx in range(0, len(json_dict)):
+        if json_dict[idx]['completed']:
             print('\t {}'.format(json_dict[idx]['title']))
 
 
 if __name__ == "__main__":
     if len(argv) == 2:
-        usr(argv=argv[1])
+        get_user(argv=argv[1])
