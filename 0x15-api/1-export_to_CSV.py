8809848b39dca9403ebf27c7b3d8d816c058ebcc
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-23 16:46:35 -0700

    mandatory tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""this model is my firt time using REST API"""
+"""Export to CSV"""
 import csv
 import json
 from sys import argv
 from urllib import request
 
 
-def to_csv(argv):
-    tsks_id = request.urlopen(
+def csv(argv):
+    tasks_id = request.urlopen(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
-    user_name = request.urlopen(
+    usr_name = request.urlopen(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
-    res_body = tsks_id.read()
-    user_body = user_name.read()
+    res_body = tasks_id.read()
+    user_body = usr_name.read()
     json_dict = json.loads(res_body.decode("utf-8"))
     json_user = json.loads(user_body.decode("utf-8"))
     file_name = '{}.csv'.format(argv)
@@ -27,4 +27,4 @@ def to_csv(argv):
 
 
 if __name__ == "__main__":
-    to_csv(argv=argv[1])
+    csv(argv=argv[1])
