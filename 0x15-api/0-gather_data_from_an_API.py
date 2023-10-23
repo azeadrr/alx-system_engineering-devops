264d6f1ee32e21cecb82060e0b401f354205db4d
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-23 15:17:41 -0700

    mandatory tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,15 +8,17 @@ from urllib import request
 
 def get_user(argv):
     """Gather data from an API"""
-    tasksid = request.urlopen('https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
-    userid = request.urlopen('https://jsonplaceholder.typicode.com/users/{}'.format(argv))
+    tasksid = request.urlopen(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
+    userid = request.urlopen(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
     res = tasksid.read()
     user = userid.read()
     json_dct = json.loads(res.decode("utf-8"))
     json_usr = json.loads(user.decode("utf-8"))
     count = 0
-    for index in range(0, len(json_dct)):
-        if not json_dct[index]['completed']:
+    for i in range(0, len(json_dct)):
+        if not json_dct[i]['completed']:
             count += 1
     print('Employee {} is done with tasks({}/{}):'
           .format(json_usr['name'], len(json_dct) - count, len(json_dct)))
