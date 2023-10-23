9a08b0f9dda9cecfbf9ff79125315e49a2eb2050
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-23 15:26:12 -0700

    mandatory tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,28 +5,27 @@ import json
 from sys import argv
 from urllib import request
 
-
-def get_user(argv):
+def usr(argv):
     """Gather data from an API"""
     tasksid = request.urlopen(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
     userid = request.urlopen(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
     res = tasksid.read()
     user = userid.read()
-    json_dct = json.loads(res.decode("utf-8"))
-    json_usr = json.loads(user.decode("utf-8"))
+    json_dict = json.loads(res.decode("utf-8"))
+    json_user = json.loads(user.decode("utf-8"))
     count = 0
-    for i in range(0, len(json_dct)):
-        if not json_dct[i]['completed']:
+    for i in range(0, len(json_dict)):
+        if not json_dict[i]['completed']:
             count += 1
-    print('Employee {} is done with tasks({}/{}):'
-          .format(json_usr['name'], len(json_dct) - count, len(json_dct)))
-    for id in range(0, len(json_dct)):
-        if json_dct[id]['completed']:
-            print('\t {}'.format(json_dct[id]['title']))
+            print('Employee {} is done with tasks({}/{}):'
+          .format(json_user['name'], len(json_dict) - count, len(json_dict)))
+    for idx in range(0, len(json_dict)):
+        if json_dict[idx]['completed']:
+            print('\t {}'.format(json_dict[idx]['title']))
 
 
 if __name__ == "__main__":
     if len(argv) == 2:
-        get_user(argv=argv[1])
+        usr(argv=argv[1])
