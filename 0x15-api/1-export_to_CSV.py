80095883d249482eff40761274626905796aea26
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-23 17:28:57 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Export to CSV"""
+"""Python script to export data in the CSV"""
 import csv
 import json
 from sys import argv
 from urllib import request
 
 
-def to_csv(argv):
-    tsks_id = request.urlopen(
+def expo_to_csv(argv):
+    """Export to CSV"""
+    tasks_id = request.urlopen(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv))
-    user_name = request.urlopen(
+    usr_name = request.urlopen(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv))
-    res_body = tsks_id.read()
-    user_body = user_name.read()
+    res_body = tasks_id.read()
+    user_body = usr_name.read()
     json_dict = json.loads(res_body.decode("utf-8"))
     json_user = json.loads(user_body.decode("utf-8"))
     file_name = '{}.csv'.format(argv)
     with open(file_name, 'w') as csv_f:
         writer = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
-
         for idx in range(0, len(json_dict)):
             writer.writerow([json_user['id'], json_user['username'],
                              json_dict[idx]['completed'],
@@ -27,4 +27,4 @@ def to_csv(argv):
 
 
 if __name__ == "__main__":
-    to_csv(argv=argv[1])
+    expo_to_csv(argv=argv[1])
