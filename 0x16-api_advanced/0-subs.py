e7855338ca893b482f263860e3d22b2df2bda05b
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 12:47:16 -0800

    mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API"""
+""" function that executes an api get request """
 from requests import get
 
+
 def number_of_subscribers(subreddit):
-    """queries Reddit API and returns number of subs"""
+    """ api request to get number of subscribers """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    head = {
+    lien = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
                 Gecko/20100101 Firefox/108.0"
     }
-    resp = get(link, head=head).json()
-    subscribers = resp.get('data', {}).get('subscribers', 0)
+    response = get(lien, headers=headers).json()
+    subscribers = response.get('data', {}).get('subscribers', 0)
     return subscribers
