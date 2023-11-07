b49da39cb63118ee1cea697dd9e36d87d5079e19
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 12:45:59 -0800

    mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,15 @@
 """function that queries the Reddit API"""
 from requests import get
 
-
 def number_of_subscribers(subreddit):
-    """ function that queries Reddit API and returns number of subs """
+    """queries Reddit API and returns number of subs"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     head = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \Gecko/20100101 Firefox/108.0"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
+                Gecko/20100101 Firefox/108.0"
     }
     resp = get(link, head=head).json()
-    subs = resp.get('data', {}).get('subscribers', 0)
-    return subs
+    subscribers = resp.get('data', {}).get('subscribers', 0)
+    return subscribers
