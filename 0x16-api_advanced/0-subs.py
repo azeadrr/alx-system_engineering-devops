e2aa3328bf81a3a3933a7d10aa5719936917c862
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 12:57:16 -0800

    mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ def number_of_subscribers(subreddit):
         return 0
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \ Gecko/20100101 Firefox/108.0"
     }
     resp = get(link, headers=headers).json()
     subscribers = resp.get('data', {}).get('subscribers', 0)
