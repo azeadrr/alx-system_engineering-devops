48142122a926bccf92a3c21e09325c24a52ca2aa
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 15:41:01 -0800

    mandatory

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,24 +4,20 @@ from requests import get
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API
-    """
-    if subreddit is None:
-        return 0
-    url_base = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
-    }
-    params = {
-            "limit": 10
-            }
-    resp = get(url_base, params=params, headers=headers,
-                   allow_redirects=False).json()
-    children = resp.get("data", {}).get("children", None)
-    if children:
-        for subject in children:
-            print(subject.get("data").get("title"))
+    """prints titles of first 10 hot posts"""
+    
+    url_base = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'MyBot/1.0 (by /u/bouhvli)'}
+
+    resp = requests.get(url_base, headers=headers)
+    if resp.status_code == 200:
+        try:
+            dt = resp.json()
+            posts = dt['data']['children']
+            for i, post in enumerate(posts):
+                title = post['data']['title']
+                print("{}. {}".format(i + 1 - 1, title))
+        except(KeyError):
+            print("None")
     else:
         print("None")
