018a04478482319b003fcd11093cd32b85ea4b0e
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 12:55:31 -0800

    mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" function that queries the Reddit API and returns the number of subscribers """
+""" queries Reddit API and returns number of subs """
 from requests import get
 
 
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     """ get number of subscribers """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    lien = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
                 Gecko/20100101 Firefox/108.0"
     }
-    response = get(lien, headers=headers).json()
-    subscribers = response.get('data', {}).get('subscribers', 0)
+    resp = get(link, headers=headers).json()
+    subscribers = resp.get('data', {}).get('subscribers', 0)
     return subscribers
