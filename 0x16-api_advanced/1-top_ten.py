df413bc4390bffa49b2a5ecb6a25afc58f33c2c8
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 15:51:01 -0800

    mandatory

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,10 @@ def top_ten(subreddit):
     if subreddit is None:
         return 0
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'MyBot/1.0 (by /u/azeadrr)'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
+                Gecko/20100101 Firefox/108.0"
+    }
     params = {
             "limit": 10
             }
