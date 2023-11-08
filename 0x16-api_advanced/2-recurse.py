b8364ae042a70fe35e08ad2870416a2df3327880
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-08 13:56:45 -0800

    mandatory task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,21 +13,16 @@ def recurse(subreddit, hot_list=[], after="", count=0):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
                 Gecko/20100101 Firefox/108.0"
     }
-    params = {
-            "limit": 100,
-            "after": after,
-            "count": count
-    }
-    response = get(url_base, params=params,
-                   headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return None
-    resp = response.json().get("data")
-    after = resp.get("after")
-    count += resp.get("dist")
-    for child in resp.get("children"):
-        hot_list.append(child.get("data").get("title"))
+    response = requests.get(url_base, headers=headers, params={"after": after})
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        return None
