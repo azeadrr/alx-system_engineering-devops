37cb1a5118e03cfa103a3812c98b90fc02e4f8be
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 16:14:45 -0800

    mandatory

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""sumary_line
-"""
-
-import requests
+"""function that queries the Reddit API"""
+import requests import get
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """sumary_line
-    """
+    """function that queries the Reddit API"""
 
-    url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url_base = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Custom)'}
-    response = requests.get(url, headers=headers, params={"after": after})
+    resp = requests.get(url_base, headers=headers, params={"after": after})
 
-    if response.status_code == 200:
-        data = response.json()
-        for post in data['data']['children']:
-            hot_list.append(post['data']['title'])
+    if resp.status_code == 200:
+        data = resp.json()
+        for pst in data['data']['children']:
+            hot_list.append(pst['data']['title'])
         after = data['data']['after']
         if after is None:
             return hot_list
