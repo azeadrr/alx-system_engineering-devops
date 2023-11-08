0f90fc3473f65ea7daed153dc31202685642619a
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-07 16:10:06 -0800

    mandatory

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""sumary_line
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """sumary_line
+    """
+
+    url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Custom)'}
+    response = requests.get(url, headers=headers, params={"after": after})
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        return None
