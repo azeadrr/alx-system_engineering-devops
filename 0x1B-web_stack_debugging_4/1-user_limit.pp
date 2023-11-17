094a0472cdd061e9347e7db258eabd25f24a4754
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-11-16 17:00:13 -0800

    updated

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+exec { 'hardware-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'software-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
